dd925ba005697a7ac088a659e12a746e2aa3528c
Debarghya Sikder <debarghya1412@gmail.com>
2025-05-17 03:59:27 +0530

    data transformation

--- Insurance/components/data_transformation.py
+++ Insurance/components/data_transformation.py
@@ -1,14 +1,3 @@
-import os,sys
-from Insurance.exception import InsuranceException
-from Insurance.logger import logging
-from datetime import datetime
-
-
-FILE_NAME = "insurance.csv"
-TRAIN_FILE_NAME = "train.csv"
-TEST_FILE_NAME = "test.csv"
-TRANSFORMER_OBJECT_FILE_NAME = "transformer.pkl"
-TARGET_ENCODER_OBJECT_FILE_NAME = "target_encoder.pkl"
 from Insurance.entity import artifact_entity,config_entity
 from Insurance.exception import InsuranceException
 from Insurance.logger import logging
@@ -38,7 +27,6 @@ class DataTransformation:
             self.data_transformation_config=data_transformation_config
             self.data_ingestion_artifact=data_ingestion_artifact
         except Exception as e:
-            logging.error(f"Exception occurred: {e}", exc_info=True)
             raise InsuranceException(e, sys)
 
 
@@ -117,52 +105,3 @@ class DataTransformation:
             return data_transformation_artifact
         except Exception as e:
             raise InsuranceException(e, sys)
-class TrainingPipelineConfig:
-    
-    def __init__(self):
-        try:
-            self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H%M%S')}")
-        except Exception  as e:
-            raise InsuranceException(e,sys)    
-
-
-class DataIngestionConfig:
-    
-    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-        try:
-            self.database_name="INSURANCE"
-            self.collection_name="INSURANCE_PROJECT"
-            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
-            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
-            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
-            self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
-            self.test_size = 0.2
-        except Exception  as e:
-            raise InsuranceException(e,sys)      
-
-            
-# Convert data into dict
-    def to_dict(self,)->dict:
-        try:
-            return self.__dict__
-        except Exception  as e:
-            raise InsuranceException(e,sys)          
-
-class DataValidationConfig:
-    
-    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-        self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir , "data_validation")
-        self.report_file_path=os.path.join(self.data_validation_dir, "report.yaml")
-        self.missing_threshold:float = 0.2
-        self.base_file_path = os.path.join("insurance.csv")
-
-
-
-class DataTransformationConfig:
-    
-    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-        self.data_transformation_dir = os.path.join(training_pipeline_config.artifact_dir , "data_transformation")
-        self.transform_object_path = os.path.join(self.data_transformation_dir,"transformer",TRANSFORMER_OBJECT_FILE_NAME)
-        self.transformed_train_path =  os.path.join(self.data_transformation_dir,"transformed",TRAIN_FILE_NAME.replace("csv","npz"))
-        self.transformed_test_path =os.path.join(self.data_transformation_dir,"transformed",TEST_FILE_NAME.replace("csv","npz"))
-        self.target_encoder_path = os.path.join(self.data_transformation_dir,"target_encoder",TARGET_ENCODER_OBJECT_FILE_NAME)
